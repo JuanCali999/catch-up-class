9d229c482318e5c8f0741b70898bda4e1af57a4d
Juan Calisaya <u202121935@upc.edu.pe>
2024-10-03 05:20:27 -0500

    feat(footer): implemented footer section.

---
+++ src/app/public/components/footer-content/footer-content.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-footer-content',
+  standalone: true,
+  imports: [
+    TranslateModule,
+    MatIcon
+  ],
+  templateUrl: './footer-content.component.html',
+  styleUrl: './footer-content.component.css'
+})
+export class FooterContentComponent {
+
+}
+
